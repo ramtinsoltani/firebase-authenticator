74894f2603517a7adf626e083690de79f316447f
Ramtin Soltani <ramtinsoltani93@gmail.com>
2018-04-05 02:06:21 -0700

    Added Facebook login

--- public/script.js
+++ public/script.js
@@ -4,7 +4,30 @@
 
 })();
 
-function googleSignin() {
+function facebookLogin() {
+
+  firebase.auth().signInWithPopup(new firebase.auth.FacebookAuthProvider())
+  .then(result => {
+
+    return result.user.getIdToken();
+
+  })
+  .then(token => {
+
+    const output = document.getElementById('token');
+
+    output.value = token;
+
+  })
+  .catch(error => {
+
+    console.error(error);
+
+  });
+
+}
+
+function googleLogin() {
 
   firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
   .then(result => {
